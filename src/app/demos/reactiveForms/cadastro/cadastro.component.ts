de8f461124bf383600523a356a9b9a6541cd1771
marcusfcbarbosa <marcusfernandocbarbosa@gmail.com>
2023-04-02 23:16:54 -0300

    Fazendo mais validacoes e pegando validacoes especificas como especificas de e-mail

--- src/app/demos/reactiveForms/cadastro/cadastro.component.ts
+++ src/app/demos/reactiveForms/cadastro/cadastro.component.ts
@@ -10,27 +10,27 @@ export class CadastroComponent implements OnInit {
 
   cadastroForm: FormGroup;
   usuario: Usuario;
-  formResult:string;
-  constructor(private fb: FormBuilder) {}
+  formResult: string;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.cadastroForm = this.fb.group({
-      nome: ['',Validators.required],
-      cpf: ['',Validators.required],
-      email: ['',Validators.required],
-      senha: ['',Validators.required],
-      senhaConfirmacao: ['',Validators.required]
+      nome: ['', Validators.required],
+      cpf: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', Validators.required],
+      senhaConfirmacao: ['', Validators.required]
     });
   }
 
 
   adicionarUsuario() {
-    if(this.cadastroForm.dirty &&this.cadastroForm.valid ){
-      this.usuario = Object.assign({},this.usuario,this.cadastroForm.value);
+    if (this.cadastroForm.dirty && this.cadastroForm.valid) {
+      this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value);
       this.formResult = JSON.stringify(this.cadastroForm.value);
-    }else{
-      this.formResult ='Cadastro Invalido'
+    } else {
+      this.formResult = 'Cadastro Invalido'
     }
-    
+
   }
 }
