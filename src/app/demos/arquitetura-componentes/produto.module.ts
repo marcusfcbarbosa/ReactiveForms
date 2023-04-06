985ca0cdd72dc9e0e6532c964c9dce6adb0cee68
marcusfcbarbosa <marcusfernandocbarbosa@gmail.com>
2023-04-05 23:31:42 -0300

    Componente Filho se comunicando com Componente Pai

--- src/app/demos/arquitetura-componentes/produto.module.ts
+++ src/app/demos/arquitetura-componentes/produto.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from "@angular/common";// para definir que esse não é 
 import { ProdutoDashboardComponent } from "./produto-dashboard/produto-dashboard.component";
 import { ProdutoRoutingModule } from "./produto.route";
 
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { ProdutoDetalheComponent } from "./componentes/produto-card-detalhe.componente";
+
+registerLocaleData(localePt);
 
 @NgModule({
 
     declarations: [
-        ProdutoDashboardComponent
+        ProdutoDashboardComponent,
+        ProdutoDetalheComponent
     ],
     imports: [
         CommonModule,
