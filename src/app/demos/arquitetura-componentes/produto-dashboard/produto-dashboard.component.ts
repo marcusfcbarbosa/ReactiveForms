8213b20b71c40c8d90a90c49d0adf7fc8601dc3b
marcusfcbarbosa <marcusfernandocbarbosa@gmail.com>
2023-04-07 21:19:26 -0300

    Interagir com eventos do HTML , por exemplo evento click obter uma coleção ou item através do Decorator @ViewChildren ou @ViewChild

--- src/app/demos/arquitetura-componentes/produto-dashboard/produto-dashboard.component.ts
+++ src/app/demos/arquitetura-componentes/produto-dashboard/produto-dashboard.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { Produto } from '../models/produto';
+import { Observable, fromEvent } from 'rxjs';
+import { ProdutoCountComponent } from '../componentes/produto-count.componente';
+import { ProdutoDetalheComponent } from '../componentes/produto-card-detalhe.componente';
 
 @Component({
   selector: 'app-produto-dashboard',
@@ -7,9 +10,16 @@ import { Produto } from '../models/produto';
   styles: []
 })
 
-export class ProdutoDashboardComponent implements OnInit {
+export class ProdutoDashboardComponent implements OnInit, AfterViewInit {
   produtos: Produto[]
+
+  @ViewChild(ProdutoCountComponent, { static: false }) contador: ProdutoCountComponent;
+  @ViewChild('teste', { static: false }) mensagemTela: ElementRef;
+
+
+  @ViewChildren(ProdutoDetalheComponent) botoes: QueryList<ProdutoDetalheComponent>;
   constructor() { }
+
 
   ngOnInit() {
     this.produtos = [{
@@ -54,6 +64,24 @@ export class ProdutoDashboardComponent implements OnInit {
       valor: 600,
       imagem: 'headset.jpg'
     }];
+  }
+
+  ngAfterViewInit(): void {
+    //se cair aqui é pq a view ja foi renderizada //isso daqui é o equivalente ao $(#id).click do jquery
+    let clickTexto: Observable<any> = fromEvent(this.mensagemTela.nativeElement, 'click');
+    clickTexto.subscribe(() => {
+      alert('clickou no Texto');
+      return;
+    });
+
+    //ViewChild
+    console.log('Objeto do Contador:', this.contador.produtos);
+
+    //ViewChildren
+    this.botoes.forEach(p=>{
+      console.log('Log para cada produto do ProdutoDetalheComponent :');
+      console.log( p.produto);
+    });
   }
 
   mudarStatus(event: Produto) {
